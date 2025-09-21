78f0a8c1bf5d2059cb0004574c797e7b2564c15c
hesspet <hesspet@gmx.de>
2025-09-21 20:31:30 +0200

    Add display orientation support to DisplayManager

--- Src/Esp32NMCI/src/Presentation/DisplayManager.cpp
+++ Src/Esp32NMCI/src/Presentation/DisplayManager.cpp
@@ -3,10 +3,26 @@
 #include "DisplayManager.h"
 #include "../Logger/Logger.h"
 
-void DisplayManager::begin()
+void DisplayManager::begin(Orientation orientation)
 {
     tft.init();
-    tft.setRotation(1);
+
+    // Map the requested physical orientation to the library's rotation indices.
+    uint8_t rotation = 1;
+    switch (orientation)
+    {
+        case Orientation::UsbRight:
+            rotation = 1;
+            break;
+        case Orientation::UsbLeft:
+            rotation = 3;
+            break;
+        default:
+            rotation = 1;
+            break;
+    }
+
+    tft.setRotation(rotation);
 
 #ifdef TFT_BL
     pinMode(TFT_BL, OUTPUT);
@@ -16,7 +32,8 @@ void DisplayManager::begin()
     showMessage("Hello", false);
 }
 
-void DisplayManager::showMessage(const String& message, bool withOverlay) {
+void DisplayManager::showMessage(const String& message, bool withOverlay)
+{
     tft.fillScreen(TFT_BLACK);
     tft.setTextDatum(MC_DATUM);  // Mitte zentriert
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
@@ -40,5 +57,5 @@ void DisplayManager::showMessage(const String& message, bool withOverlay) {
     tft.setTextSize(bestSize);
     tft.drawString(message, tft.width() / 2, tft.height() / 2);
 
-    // drawStatusOverlay();  // Sprite neu zeichnen}
+    // drawStatusOverlay();  // Sprite neu zeichnen
 }
